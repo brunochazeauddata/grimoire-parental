57688bdd8243063a47a56274f7bea02058f47fa7
brunochazeauddata <bruno.chazeaud.data@gmail.com>
2026-07-14 16:43:53 +0200

    Update emails.ts

--- convex/emails.ts
+++ convex/emails.ts
@@ -2,15 +2,17 @@ import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const saveEmail = mutation({
-  args: {
-    email: v.string(),
-    name: v.string(),
-    timestamp: v.string(),
+  args: { 
+    email: v.string(), 
+    name: v.string(), 
+    childrenCount: v.optional(v.string()), // <--- AJOUTE CETTE LIGNE
+    timestamp: v.string() 
   },
   handler: async (ctx, args) => {
-    await ctx.db.insert("emails", {
+    await ctx.db.insert("emails", { // ou le nom de ta table
       email: args.email,
       name: args.name,
+      childrenCount: args.childrenCount, // <--- AJOUTE CETTE LIGNE
       timestamp: args.timestamp,
     });
   },
